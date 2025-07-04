97746ad3b00920da7131f44ada925fbf942e83af
Kenan <yahyayevkenan57@gmail.com>
2025-07-04 16:52:01 +0400

    Update 9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,39 +1,39 @@
 #include "main.h"
 
 /**
- * times_table - prints the 9 times table, starting with 0
+ * times_table - Prints the 9 times table, starting with 0.
  */
 void times_table(void)
 {
-    int row, col, product;
+	int row, col, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (col = 0; col <= 9; col++)
-        {
-            product = row * col;
+	for (row = 0; row <= 9; row++)
+	{
+		for (col = 0; col <= 9; col++)
+		{
+			product = row * col;
 
-            if (col == 0)
-            {
-                _putchar(product + '0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
+			if (col == 0)
+			{
+				_putchar(product + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 
-                if (product < 10)
-                {
-                    _putchar(' ');
-                    _putchar(product + '0');
-                }
-                else
-                {
-                    _putchar((product / 10) + '0');
-                    _putchar((product % 10) + '0');
-                }
-            }
-        }
-        _putchar('\n');
-    }
+				if (product < 10)
+				{
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+				else
+				{
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
 }
