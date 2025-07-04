36d0403f9103bb9178d220750ad1cf36753f7049
Kenan <yahyayevkenan57@gmail.com>
2025-07-04 16:24:53 +0400

    Update 8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
 /**
- * main - prints numbers from 0 to 9 and letters a to f in lowercase, followed by a new line
+ * main - Prints numbers from 0 to 9 and letters a to f in lowercase,
+ *        followed by a new line
  *
  * Return: Always 0
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 16; i++)
-    {
-        if (i < 10)
-            putchar(i + '0');
-        else
-            putchar(i - 10 + 'a');
-    }
-    putchar('\n');
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10)
+		{
+			putchar(i + '0');
+		}
+		else
+		{
+			putchar(i - 10 + 'a');
+		}
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
