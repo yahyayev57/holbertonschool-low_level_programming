bdc4a767368bad47ddcacfb7238530d5bab1b102
Kenan <yahyayevkenan57@gmail.com>
2025-07-04 12:41:01 +0400

    Create 6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 9 followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 0; i <= 9; i++)
+        putchar(i + '0');
+    putchar('\n');
+
+    return (0);
+}
