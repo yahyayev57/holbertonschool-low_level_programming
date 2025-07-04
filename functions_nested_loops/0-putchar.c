e5bf81c4a08812436397a326e5be8a9febdb6cc2
Kenan <yahyayevkenan57@gmail.com>
2025-07-04 12:53:49 +0400

    Update 0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - prints _putchar followed by a new line
+ * main - prints _putchar, followed by a new line
  *
  * Return: Always 0
  */
 int main(void)
 {
-    printf("_putchar\n");
+    char str[] = "_putchar";
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+        _putchar(str[i]);
+    _putchar('\n');
+
     return (0);
 }
