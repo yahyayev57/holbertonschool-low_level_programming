70cd0cb6b3ddf4c2538676fc4e764bb5d7802263
Kenan <yahyayevkenan57@gmail.com>
2025-07-04 13:12:37 +0400

    Update main.h

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,11 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-
-int _islower(int c);
 int _putchar(char c);
-int _isalpha(int c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
+int print_sign(int n);
 
 #endif /* MAIN_H */
