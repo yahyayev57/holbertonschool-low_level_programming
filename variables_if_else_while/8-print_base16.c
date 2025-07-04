db3916aa7aecba1c5191ac8212bc98550dafc4af
Kenan <yahyayevkenan57@gmail.com>
2025-07-04 12:42:18 +0400

    Create 8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 to 9 and letters a to f in lowercase, followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 16; i++)
+    {
+        if (i < 10)
+            putchar(i + '0');
+        else
+            putchar(i - 10 + 'a');
+    }
+    putchar('\n');
+
+    return (0);
+}
