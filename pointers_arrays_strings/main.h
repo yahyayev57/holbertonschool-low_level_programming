ea5454f4f256af9228d86f138ce93b56dbc38a7a
Kenan <yahyayevkenan57@gmail.com>
2025-07-05 18:03:56 +0400

    Update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -24,6 +24,7 @@ char *cap_string(char *str);
 char *leet(char *str);
 
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 
 #endif /* MAIN_H */
