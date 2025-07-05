d7457de92515c18cd46d30fa1e69149b81a7f7cc
Kenan <yahyayevkenan57@gmail.com>
2025-07-05 18:04:54 +0400

    Create 2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search in
+ * @c: character to locate
+ *
+ * Return: pointer to the first occurrence of c in s,
+ *         or NULL if c is not found
+ */
+char	*_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (s + i);
+		i++;
+	}
+	/* Check if c is '\0', return pointer to the null terminator */
+	if (c == '\0')
+		return (s + i);
+
+	return (NULL);
+}
