66b00b7631ae98c786235a3c57485173697ea9e2
Kenan <yahyayevkenan57@gmail.com>
2025-07-05 18:06:57 +0400

    Update main.h

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -26,6 +26,7 @@ char *leet(char *str);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 
 
 #endif /* MAIN_H */
