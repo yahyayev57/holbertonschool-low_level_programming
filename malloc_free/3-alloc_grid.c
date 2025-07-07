de9c236817f6c123f669a0fe916abf95a9349f10
Kenan <yahyayevkenan57@gmail.com>
2025-07-07 19:38:46 +0400

    Update 3-alloc_grid.c

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -6,9 +6,9 @@
  * @width: width of the grid
  * @height: height of the grid
  *
- * Return: pointer to the grid, or NULL on failure or if width or height is <= 0
+ * Return: pointer to the grid, or NULL on failure
+ *         or if width or height is <= 0
  */
-
 int **alloc_grid(int width, int height)
 {
 	int **grid;
@@ -17,25 +17,20 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	/* Allocate memory for rows (array of int pointers) */
 	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
 		return (NULL);
 
-	/* Allocate memory for each row and initialize to 0 */
 	for (i = 0; i < height; i++)
 	{
 		grid[i] = malloc(sizeof(int) * width);
 		if (grid[i] == NULL)
 		{
-			/* On failure, free already allocated rows */
 			for (j = 0; j < i; j++)
 				free(grid[j]);
 			free(grid);
 			return (NULL);
 		}
-
-		/* Initialize each element of the row to 0 */
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
 	}
