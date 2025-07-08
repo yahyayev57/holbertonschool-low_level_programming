20e965d3c8267cf2e784852bfd02207d07d6bbea
Kenan <yahyayevkenan57@gmail.com>
2025-07-08 13:53:36 +0400

    Create main_0.c

---
+++ structures_typedef/main_0.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "dog.h"
+
+int main(void)
+{
+	struct dog my_dog;
+
+	init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
+	printf("My name is %s, and I am %.2f :) - Woof!\n",
+	       my_dog.name, my_dog.age);
+	return (0);
+}
