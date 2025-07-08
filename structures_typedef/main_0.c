b51bffe812593017a8b7374e094a3926a61b507a
Kenan <yahyayevkenan57@gmail.com>
2025-07-08 14:05:00 +0400

    Update main_0.c

--- structures_typedef/main_0.c
+++ structures_typedef/main_0.c
@@ -3,15 +3,12 @@
 
 int main(void)
 {
-    dog_t *my_dog;
+	dog_t *my_dog;
 
-    my_dog = new_dog("Ghost", 4.75, "Jon Snow");
-    if (my_dog == NULL)
-        return (1);
+	my_dog = new_dog("Ghost", 4.75, "Jon Snow");
+	printf("My name is %s, I am %.2f, and my owner is %s\n",
+	       my_dog->name, my_dog->age, my_dog->owner);
+	free_dog(my_dog);
 
-    printf("My name is %s, and I am %.2f :) - Woof!\n", my_dog->name, my_dog->age);
-
-    /* Remember to free allocated memory here */
-
-    return (0);
+	return (0);
 }
