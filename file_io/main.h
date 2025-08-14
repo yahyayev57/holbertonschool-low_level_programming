f8bd75a70ffd9a429d99ba44152c4e60365ce297
Kenan <yahyayevkenan57@gmail.com>
2025-08-14 14:38:18 +0400

    Update main.h

--- file_io/main.h
+++ file_io/main.h
@@ -1,9 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <sys/types.h>
 #include <stddef.h>
+#include <sys/types.h>
 
+int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif /* MAIN_H */
