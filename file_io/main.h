75ff6692a8dc7c35167c716d6d461469eaa15c6b
Kenan <yahyayevkenan57@gmail.com>
2025-08-14 14:41:26 +0400

    Update main.h

--- file_io/main.h
+++ file_io/main.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <sys/types.h>
+#define BUFFER_SIZE 1024
 
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
