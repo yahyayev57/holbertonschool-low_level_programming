cd3e7afde9259751c55cfb0462483ddf6b610746
Kenan <yahyayevkenan57@gmail.com>
2025-08-14 14:35:07 +0400

    Create 0-read_textfile.c

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * @filename: Name of the file to read.
+ * @letters: Number of letters to read and print.
+ *
+ * Return: Actual number of letters it could read and print.
+ *         0 on failure or if filename is NULL.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	r = read(fd, buffer, letters);
+	if (r == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	w = write(STDOUT_FILENO, buffer, r);
+	free(buffer);
+	close(fd);
+
+	if (w != r)
+		return (0);
+
+	return (w);
+}
