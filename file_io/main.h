fd534fb48ccefcea0897fe340a9abc917f90249f
Kenan <yahyayevkenan57@gmail.com>
2025-08-17 16:37:11 +0400

    Update main.h

--- file_io/main.h
+++ file_io/main.h
@@ -8,5 +8,6 @@
 int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
 
 #endif /* MAIN_H */
